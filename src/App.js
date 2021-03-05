372f28f8f8fc12268c922dcd7f865c100b47632f
gyo-gyo <gyo.gyo.xxxx.16@gmail.com>
2021-03-05 21:55:14 +0100

    first-react-practice-file

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() { 
+    return <div>
+      <h1>React</h1>
+      <p>this is sample componete</p>
+      <p>これはサンプルのコンポネートです。</p>
+
+
+
+
+    </div>
+      
+
+
+      }
+
+
+
+      }
+
+      export default App;
